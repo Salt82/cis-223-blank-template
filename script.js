203108ea459a521e1067c17d3919d68aafe5646b
Mona Lisa <loera.o@stu.jjc.edu>
2024-11-15 21:19:19 -0600

    creates a function that when ran will prompt the user with a question, it will save this and then have the synthesis model utter it

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ let buttonMakker = document.createElement("BUTTON");
 buttonMakker.innerText="Synthesize";
 
 document.body.appendChild(buttonMakker);
+
+function pressSpeak()
+{
+    let inputText = prompt("What text should be synthesised?")
+
+    const utterThis = new SpeechSynthesisUtterance(inputText);
+
+    speechSynthesis.speak(utterThis);
+}
