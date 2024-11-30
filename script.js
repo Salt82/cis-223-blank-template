c570fa3d9bccd95755d2dfaace134f16bc5ec78b
Salt82 <loera.o@stu.jjc.edu>
2024-11-30 13:47:27 -0600

    creates the listen to user function and the listenButton which use the speech recognition api

--- script.js
+++ script.js
@@ -3,11 +3,6 @@
 
 let buttonMakker = document.createElement("BUTTON");
 
-
-
-
-
-
 buttonMakker.innerText="Synthesize";
 
 let textBox = document.createElement("textarea");
@@ -27,11 +22,6 @@ textBox.type = "text";
 textBox.id = "textBox";
 
 textBox.placeholder = "Text to be synthesized";
-
-
-
-
-
 
 let blankSpace = document.createElement("div");
 
@@ -64,16 +54,50 @@ volumeSlider.max = 1;
 
 volumeSlider.step = .1;
 
-
 let blankSpace2 = document.createElement("div");
 
 
+let listenButton = document.createElement("BUTTON");
+
+listenButton.innerText="Want me to Listen?";
 
 
+// Creates a recognition object that listens to what the user has to say. Takes what the user says to is and
+// prints it into the text box, it then calls the pressSpeak function which makes it speak.
+
+// Code Provided By Mozilla Developer Netowrk!
+function listenToUser() {
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const recognition = new SpeechRecognition();
+    recognition.continuous = false;
+    recognition.lang = "en-US";
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 1;
+
+    const diagnostic = document.querySelector(".output");
+
+    // starts starts the recognition object, which allows it to listen to what the user is saying
+    recognition.start();
 
 
+    // on the even that the recognition object understands the user it then creates the event function 
+    recognition.onresult = (event) => {
 
+        // saves the recognized text
+        const speechResult = event.results[0][0].transcript; 
+        
+        // takes the users speech and places it in the text box
+        textBox.value = speechResult;
 
+        // calls the press speak function
+        pressSpeak()
+    };
+
+    
+}
+
+// Attach the listener to the button
+listenButton.addEventListener("click", listenToUser);
 
 
 
@@ -100,11 +124,6 @@ buttonMakker.addEventListener("click",pressSpeak);
 
 
 
-
-
-
-
-
 function backgroundColor()
 {
     document.body.style.backgroundColor = "grey"
@@ -124,6 +143,8 @@ document.querySelector("p").appendChild(textBox);
 document.querySelector("p").appendChild(blankSpace);
 
 document.querySelector("p").appendChild(buttonMakker);
+
+document.querySelector("p").appendChild(listenButton);
 
 document.querySelector("p").appendChild(blankSpace2);
 
